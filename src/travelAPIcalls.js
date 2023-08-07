c796c051e5e853bc6f4f575006ae909aa0ae3952
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-08-07 19:19:43 -0400

    Add single use fetch to fix bug

--- src/travelAPIcalls.js
+++ src/travelAPIcalls.js
@@ -6,38 +6,36 @@ export const fetchUserBookings = (data) => {
   );
 };
 
+export const singleFetchRequest = (url) => {
+  return fetch(url)
+  .then(response => response.json())
+}
+
 export const fetchAll = [
   fetchUserBookings("travelers"),
   fetchUserBookings("trips"),
   fetchUserBookings("destinations"),
 ];
 
-export const postUserTrips = (id, userID, destinationID, travelers, date, duration, status, suggestedActivities) => {
-  const postObject = {
-    id: id,
-    userID: userID,
-    destinationID: destinationID,
-    travelers: travelers,
-    date: date,
-    duration: duration,
-    status: status,
-    suggestedActivities: suggestedActivities
-  }
+export const postUserTrips = (postObject) => {
+  // const postObject = {
+  //   id: id,
+  //   userID: userID,
+  //   destinationID: destinationID,
+  //   travelers: travelers,
+  //   date: date,
+  //   duration: duration,
+  //   status: status,
+  //   suggestedActivities: suggestedActivities
+  // }
 
   console.log("pls", postObject)
 
   return fetch("http://localhost:3001/api/v1/trips", {
     method: "POST",
-    body: JSON.stringify({
-      id: id,
-      userID: userID,
-      destinationID: destinationID,
-      travelers: travelers,
-      date: date,
-      duration: duration,
-      status: status,
-      suggestedActivities: suggestedActivities,
-    }),
+    body: JSON.stringify(
+     postObject
+    ),
     headers: { "Content-Type": "application/json" },
   });
-};
+}
